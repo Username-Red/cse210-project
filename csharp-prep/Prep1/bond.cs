59cddbac11e4f5ea02c86514aceaa9cf1cf3960c
Username-Red <ashkestrel@gmail.com>
2023-01-07 22:08:04 +0200

    added bond

---
+++ csharp-prep/Prep1/bond.cs
@@ -0,0 +1,9 @@
+
+Console.WriteLine("What is your first name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("What is your Surname?");
+string surname = Console.ReadLine();
+
+Console.Write($"Your name is {surname}, {name} {surname}.");
+
