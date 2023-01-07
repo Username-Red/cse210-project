ccc6f90bc4da962b42c1ed791813a7ac86e78644
Username-Red <ashkestrel@gmail.com>
2023-01-07 22:42:26 +0200

    Heaven help me

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your surname?");
+        string surname = Console.ReadLine();
+
+
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
