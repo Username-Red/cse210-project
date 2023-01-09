05a2e477eef301748f4de6bf3e5b6e67aa03c6fe
Username-Red <ashkestrel@gmail.com>
2023-01-09 10:56:28 +0200

    Adding  GAME

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade pergentage?: ");
+        string grade_percentage = Console.ReadLine();
+        string grade = "blank";
+
+        int gp = int.Parse(grade_percentage);
+
+        if (gp >= 90) {
+            grade = "A";
+
+        }
+
+        else if (gp >= 80 && gp < 90) {
+
+            grade = "B";
+
+        }
+
+        else if (gp >= 70 && gp < 80) {
+
+            grade = "C";
+
+        }
+
+        else if (gp >= 60 && gp < 70) {
+
+            grade = "D";
+
+        }
+
+        else if (gp < 60) {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (gp >= 70) {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+
+        else if (gp < 70) {
+            Console.WriteLine("Awww too bad, you didn't make it. But I'm sure you'll get there next time!");
+        }
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess_count = 0;
+
+        int guess = -1;
+
+        while (guess != magicNumber) {
+
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            }
+
+            guess_count += 1;
+        }
+        Console.Write("Well done! You guessed the magic number!");
+        Console.WriteLine($"And it only took {guess_count} guesses!");
     }
 }
