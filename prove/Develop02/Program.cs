11961681d735a12f98992a1c919b8d84bce79b42
Username-Red <ashkestrel@gmail.com>
2023-01-23 17:21:10 +0200

    hope for the best

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string answer = "";
+
+        while (answer != "5") {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            answer = Console.ReadLine();
+
+            if (answer == "1") {
+                //code here
+                // we want one of our writing prompts to be displayed to the screen
+                // then we want our user to input some text
+                randomPrompt prompt1 = new randomPrompt();
+            }
+
+            else if (answer == "2") {
+
+            }
+
+            else if (answer == "3") {
+                
+            }
+
+            else if (answer == "4") {
+                
+            }
+            else if (answer == "5") {
+                
+            }
+        }
     }
 }
---
+++ prove/Develop02/write-class.cs
@@ -0,0 +1,20 @@
+
+public class randomPrompt {
+    //member variables
+    // Random rd = new Random();
+    // public int randnum = rd.Next(100,200);
+    string[] prompts = {"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?", "What is something that happened today that I am grateful for?"};
+
+
+
+
+    //call class
+    public randomPrompt() {}
+
+
+    //methods
+
+    // public void displayPrompt() {
+    //     Console.WriteLine($"{prompts[randnum]}");
+    // }
+}
