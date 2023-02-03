24708e8bd1be69ec478d942305ccf430c8ab5bfa
Username-Red <ashkestrel@gmail.com>
2023-02-03 16:01:47 +0200

    Finishing scripture memorizer

---
+++ prove/Develop03/scripture.cs
@@ -0,0 +1,129 @@
+
+
+public class Scripture {
+    // Attributes
+    private string _reference;
+    private string _text;
+
+    // Constructors
+
+    public Scripture(string reference, string text) {
+        _reference = reference;
+        _text = text;
+    }
+
+
+    // Methods
+
+    public void DisplayScripture() {
+        Console.WriteLine($"{_reference} \n{_text}");
+
+    }
+
+    public void HideWord(int randyNum) {
+
+        string[] words = _text.Split(" ");
+        int underscoreCount = 0;
+
+
+        
+
+
+        // USED FOR HIDING A WORD
+
+        string hiddenWord = words[randyNum];
+
+        int count = hiddenWord.Count();
+        char underscore = '_';
+
+        String blank = new String(underscore, count);
+
+        words[randyNum] = blank;
+
+
+
+
+
+
+
+            
+
+        // while (words[randomNum].Contains("_") == false) 
+        //     {
+
+        //     string hiddenWord = words[randomNum];
+        //     usedNums.Append(randomNum);
+
+        //     int count = hiddenWord.Count();
+        //     char underscore = '_';
+
+            
+
+        //     String blank = new String(underscore, count);
+
+        //     words[randomNum] = blank;
+
+
+        //     randomNum = rd.Next(0, words.Count());
+
+        // }
+
+
+
+
+
+
+
+
+
+        _text = "";
+
+        foreach (string word in words) {
+
+            _text = _text + word + " ";
+
+        }
+
+        foreach (string word in words) {
+            if (word.Contains("_")) {
+                underscoreCount += 1;
+
+            }
+        }
+
+        if (underscoreCount >= words.Count()) {
+            return;
+        }
+
+
+    }
+
+    public List<int> GenerateNumberList() {
+
+        string[] words = _text.Split(" ");
+        int num = 0;
+        List<int> unusedNums = new List<int>();
+
+        foreach (string word in words) {
+            unusedNums.Add(num);
+            num += 1;
+        } 
+
+        return unusedNums;
+
+
+    }
+
+    public int GenerateRandomNum(List<int> numlist) {
+        string[] words = _text.Split(" ");
+
+        Random rd = new Random();
+        int randomNum = rd.Next(0, numlist.Count());
+        int randyNum = numlist[randomNum];
+
+        return randyNum;
+
+
+
+    }
+}
