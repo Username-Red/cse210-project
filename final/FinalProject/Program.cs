2f9c7435b10c055deab17284c985210a6ddfd39b
Username-Red <ashkestrel@gmail.com>
2023-03-24 19:13:29 +0200

    Updating game

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         List<string> charInventory = new List<string>();
         Character sebastian = new Character("Sebastian", "Dark Red", 3, 1, 20, charInventory);
-        Character lucy = new Character("Lucy", "Black", 3, 5, 20, charInventory);
+        Character lucy = new Character("Lucy", "Black", 3, 2, 20, charInventory);
 
         sebastian.Introduce();
         sebastian.Talk(lucy);
